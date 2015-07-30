680ec0f0cc733f9412447c5daae80a850503f21b
James Jenkins <applesmacs@yahoo.com>
2015-07-29 23:35:52 -0400

    Sets, Cleaning, Logic problems
    Started working on a method to solve for numbers that can only be
    figured out by nifty logic (As far as I know. I wish there were another
    way...)
    Basic solving is now described in terms of sets which makes things
    simpler. Appropriate methods have been added.
    A "list to hashable" function was added which may be useful for
    "logicising".
    Added a puzzle. Various cleaning things. I think the "was this the same
    as the previous 'solve'" may actually be working now.

--- sudoku.py
+++ sudoku.py
@@ -4,6 +4,15 @@ from copy import copy, deepcopy
 import sys
 
 
+full_set = {1,2,3,4,5,6,7,8,9}
+
+def hashableIter(iterable):
+    """
+    Must support sorted()
+    """
+    import hashlib
+    return hashlib.sha1("".join(map(str,iterable)))
+
 class Puzzle:
     """
     All dimensions and indicies are (row, column)
@@ -18,15 +27,17 @@ class Puzzle:
             for c in xrange(self.dim[1]):
                 possibilities = set(self.cells[r][c])
                 if len(possibilities) > 1:
-                    possibilities -= self.getFound((r,c))
-                    
-                    # Check if this is the only cell in the cluster that can
-                    # have value x in it
-                    other = copy(possibilities)
+                    possibilities = self.getRowPossibilities(r).intersection(
+                            self.getColPossibilities(c),
+                            self.getClusterPossibilities(self.getCellCluster((r,c)))
+                            )
+
+                    # Check if this is the only cell that can have value x in it
+                    only_cluster = copy(possibilities)
                     for cell in self.getClusterCells(self.getCellCluster((r,c)), (r,c)):
-                        other -= set(cell)
-                    if len(other) == 1:
-                        possibilities = other
+                        only_cluster -= set(cell)
+                    if len(only_cluster) == 1:
+                        possibilities = only_cluster
 
                     other2 = copy(possibilities)
                     for i, cell in enumerate(self.getRowCells(r)):
@@ -43,10 +54,35 @@ class Puzzle:
                         possibilities = other3
 
                     self.cells[r][c] = list(possibilities)
-    
+
+    def logicisze(self):
+        for r,row in enumerate(self.cells):
+            sames = {}
+
+            for c,cell in enumerate(row):
+                sames[hashableIter(cell)] = sames.get(hashableIter(cell), []).append((r,c))
+
+            for key, value in sames.iteritems():
+                if len(key) == len(value):
+                    print key, value
+
+
+
+    def correct(self, group):
+        return set(map(lambda x: x[0], filter(lambda x: len(x)==1, group)))
+
+    def getRowPossibilities(self, row):
+        return full_set - self.correct(self.getRowCells(row))
+
+    def getColPossibilities(self, col):
+        return full_set - self.correct(self.getColCells(col))
+
+    def getClusterPossibilities(self, cluster):
+        return full_set - self.correct(self.getClusterCells(cluster))
+
     def getFound(self, cell_loc):
         found_values = set()
-        
+
         for cell in self.getClusterCells(self.getCellCluster(cell_loc), ignore=cell_loc):
             if len(cell) == 1:
                 found_values.update(cell)
@@ -76,7 +112,7 @@ class Puzzle:
         return cells
 
     def getRowCells(self, row):
-        return copy(self.cells[row])
+        return self.cells[row]
 
     def getColCells(self, col):
         cells = []
@@ -86,15 +122,16 @@ class Puzzle:
         return cells
 
     def prettyPrint(self):
-        for row in self.cells:
+        for r,row in enumerate(self.cells):
             for cell in row:
                 if len(cell) == 1:
                     sys.stdout.write(" {}".format(str(cell[0])))
                 else:
                     sys.stdout.write("%{}".format(len(cell)))
                 sys.stdout.write(" ")
+            sys.stdout.write(" -- "+"".join(map(str,sorted(self.getRowPossibilities(r)))))
             sys.stdout.write("\n")
-        
+
 
 def getDefaultSudokuFill(x,y):
     return range(1,11)
@@ -201,10 +238,23 @@ p6 = """
 0 2 0 0 1 0 0 3 0
 """
 
+# Making my own...
+pt = """
+1 0 0 0 0 0 0 0 0
+0 7 0 0 3 0 0 8 0
+0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0
+0 2 0 0 1 0 0 4 0
+0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0
+0 6 0 0 5 0 0 9 0
+0 0 0 0 0 0 0 0 1
+"""
+
 # 5 stars require
 # 2 cells w/2 possiblities = no other cells may have those number
 
-data = p6
+data = p4
 data = map(lambda x: map(int,x.strip().split(" ")), data.strip().split("\n"))
 
 for r in xrange(len(data)):
@@ -218,19 +268,26 @@ p.cells = copy(data)
 p.prettyPrint()
 
 last = deepcopy(p.cells)
+
 while True:
     i = raw_input(">>> ")
-    if i == "q":
+    if i.startswith("q"):
         break
+    elif i.startswith("rp"):
+        print p.getRowPossibilities(int(i.lstrip("rp")))
+    elif i.startswith("cp"):
+        print p.getColPossibilities(int(i.lstrip("cp")))
     elif i.startswith("c"):
         print p.getColCells(int(i.lstrip("c")))
     elif i.startswith("r"):
         print p.getRowCells(int(i.lstrip("r")))
+    elif i.startswith("l"):
+        p.logicisze()
     else:
         print
         p.verifyConstraints()
         p.prettyPrint()
-        print "Same as last: %s"%(p.cells == last)
+        print "Same as last: %s"%(last == p.cells)
         last = deepcopy(p.cells)
 
 #if x violates constraint:
